e2e9fca1f679e5341e8eee1186dc1b4bc8c5b9c2
Stachowiak, Bartosz <bartosz.stachowiak@aptiv.com>
2025-07-08 15:42:41 +0200

    remove gutter bg

--- init.lua
+++ init.lua
@@ -1190,6 +1190,15 @@ require('lazy').setup({
     end,
     config = function()
       require('kanagawa').setup {
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = 'none', -- Disable background for the gutter
+              },
+            },
+          },
+        },
         background = {
           dark = 'dragon',
         },
